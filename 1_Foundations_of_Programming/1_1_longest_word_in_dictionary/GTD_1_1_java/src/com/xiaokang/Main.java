8f27d298c11fc147a6bd4a8a6735e83bdd7b15ff
xiaokang <xiaokang.cao@ge.com>
2020-07-12 15:56:07 -0700

    GTD_1_1: first running java codes.

--- 1_Foundations_of_Programming/1_1_longest_word_in_dictionary/GTD_1_1_java/src/com/xiaokang/Main.java
+++ 1_Foundations_of_Programming/1_1_longest_word_in_dictionary/GTD_1_1_java/src/com/xiaokang/Main.java
@@ -1,9 +1,64 @@
 package com.xiaokang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello");
+        String targetString = "abppplee";
+
+        List<String> dictionary = new ArrayList<String>();
+        dictionary.add("able");
+        dictionary.add("ale");
+        dictionary.add("apple");
+        dictionary.add("bale");
+        dictionary.add("kangaroo");
+
+        String matchedWord =  longestWord(targetString,dictionary);
+        System.out.println("The matched word is: " + matchedWord);
+    }
+
+    /**
+     * @param targetString
+     * @param dictionary
+     * @return
+     */
+    public static String longestWord(String targetString, List<String> dictionary)
+    {
+        String matchedWord = "";
+
+        int length=0;
+        for(String word:dictionary)
+        {
+            if (isSubsequence(targetString,word) && word.length()>length)
+            {
+                length = word.length();
+                matchedWord = word;
+            }
+        }
+
+        return matchedWord;
+    }
+
+    public static boolean isSubsequence(String targetString, String testString)
+    {
+        boolean isSubsequence = false;
+
+        int i=1;
+        int j=1;
+        for(i=1;i<=testString.length();i++)
+        {
+            if(j>targetString.length()) break;
+
+            while(j<=targetString.length() && testString.charAt(i-1)!=targetString.charAt(j-1))
+            {
+                j++;
+            }
+        }
+
+        if (j<=targetString.length()) isSubsequence=true;
+
+        return isSubsequence;
     }
 }
