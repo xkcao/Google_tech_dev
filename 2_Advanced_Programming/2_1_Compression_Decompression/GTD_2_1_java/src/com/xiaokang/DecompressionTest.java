18b4f6ec481ac7b5a54e53a6be7403247f44c8f2
xiaokang <xiaokang.cao@ge.com>
2020-09-14 17:48:23 -0700

    GTD_2_1: separate to multiple test functions.

--- 2_Advanced_Programming/2_1_Compression_Decompression/GTD_2_1_java/src/com/xiaokang/DecompressionTest.java
+++ 2_Advanced_Programming/2_1_Compression_Decompression/GTD_2_1_java/src/com/xiaokang/DecompressionTest.java
@@ -8,18 +8,26 @@ import static org.junit.Assert.assertEquals;
 public class DecompressionTest {
 
     @Test
-    public void testDecompressString() {
-        String input1 = "3[abc]4[ab]c";
-        String expectedOutput1 = "abcabcabcababababc";
+    public void decompressStringTest() {
+        String input = "3[abc]4[ab]c";
+        String expectedOutput = "abcabcabcababababc";
 
-        String input2 = "10[a]";
-        String expectedOutput2 = "aaaaaaaaaa";
+        assertEquals(expectedOutput,Decompression.decompressString(input));
+    }
 
-        String input3 = "2[3[a]b";
-        String expectedOutput3 = "aaabaaab";
+    @Test
+    public void decompressStringTest2() {
+        String input = "10[a]";
+        String expectedOutput = "aaaaaaaaaa";
 
-        assertEquals(expectedOutput1,Decompression.decompressString(input1));
-        assertEquals(expectedOutput2,Decompression.decompressString(input2));
-        assertEquals(expectedOutput3,Decompression.decompressString(input3));
+        assertEquals(expectedOutput,Decompression.decompressString(input));
+    }
+
+    @Test
+    public void decompressStringTest3() {
+        String input = "2[3[a]b";
+        String expectedOutput = "aaabaaab";
+
+        assertEquals(expectedOutput,Decompression.decompressString(input));
     }
 }
