bc6faf43a612e82b580a81c9834ba9feec7ceadd
xiaokang <xiaokang.cao@ge.com>
2020-11-12 22:46:07 -0800

    GTD_2_12_CS1: exercise 1.

--- 2_Advanced_Programming/2_12_MovieReviewSentimentAnalysis/GTD_2_12_CS1/src/com/xiaokang/CS1ProjectStarterCode.java
+++ 2_Advanced_Programming/2_12_MovieReviewSentimentAnalysis/GTD_2_12_CS1/src/com/xiaokang/CS1ProjectStarterCode.java
@@ -2,6 +2,7 @@ package com.xiaokang;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /*
@@ -13,28 +14,54 @@ public class CS1ProjectStarterCode
 {
 	public static void main(String[] args) throws FileNotFoundException 
 	{
+		computeAverageScore("good");
+		computeAverageScore("fantastic");
+		computeAverageScore("extraordinary");
+		computeAverageScore("bad");
+		computeAverageScore("worst");
+		computeAverageScore("disgusting");
+	}
+
+	static double computeAverageScore(String word) throws  FileNotFoundException
+	{
 		File reviewFile = new File("data/movieReviews.txt");
 		Scanner reviewScanner = new Scanner(reviewFile);
-		Scanner keyboard = new Scanner(System.in);
 
 		int reviewScore;
+		int totalScore=0;
+		int numberAppearance=0;
+		double averageScore=0d;
 		String reviewText;
-		String word;
-
-		System.out.println("Enter a word.");
-		word = keyboard.nextLine();
 
 		while(reviewScanner.hasNext())
 		{
 			reviewScore = reviewScanner.nextInt();
 			reviewText = reviewScanner.nextLine();
 
-			if(reviewText.contains(word))
+			if(reviewText.toLowerCase().contains(word.toLowerCase()))
 			{
-				System.out.println("Score: "+reviewScore);
-				System.out.println("Text: "+reviewText);
+				//System.out.println("Score: "+reviewScore);
+				//System.out.println("Text: "+reviewText);
+
+				totalScore += reviewScore;
+				numberAppearance += 1;
 			}
 		}
+
+		if (numberAppearance>0){
+			averageScore = (double) totalScore/numberAppearance;
+			System.out.println("- " + word + " appears " + numberAppearance + " times.");
+			System.out.println("\t The average score is: " + decimalCustomFormat("##.##",averageScore));
+		}else {
+			System.out.println("This word does not appear.");
+		}
+
+		return averageScore;
 	}
 
+	static public String decimalCustomFormat(String pattern, double value ) {
+		DecimalFormat myFormatter = new DecimalFormat(pattern);
+		return myFormatter.format(value);
+		//System.out.println(value + "  " + pattern + "  " + output);
+	}
 }
