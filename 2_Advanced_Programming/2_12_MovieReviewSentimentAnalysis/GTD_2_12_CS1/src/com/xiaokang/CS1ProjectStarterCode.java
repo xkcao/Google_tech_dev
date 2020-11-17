ccbc42c6b374d23c2d78693cc647a0a936a538bb
xiaokang <xiaokang.cao@ge.com>
2020-11-16 21:51:52 -0800

    GTD_2_12_CS1: exercise 4.

--- 2_Advanced_Programming/2_12_MovieReviewSentimentAnalysis/GTD_2_12_CS1/src/com/xiaokang/CS1ProjectStarterCode.java
+++ 2_Advanced_Programming/2_12_MovieReviewSentimentAnalysis/GTD_2_12_CS1/src/com/xiaokang/CS1ProjectStarterCode.java
@@ -1,36 +1,48 @@
 package com.xiaokang;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.text.DecimalFormat;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.*;
 
-/*
+/**
  * @author: Xiaokang CAO
  */
+@SuppressWarnings("JavaDoc")
 public class CS1ProjectStarterCode
 {
-	public static void main(String[] args) throws FileNotFoundException, ExecutionException, InterruptedException {
+	public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 		scoreWordList();
 	}
 
-	static void scoreWordList() throws FileNotFoundException, ExecutionException, InterruptedException {
+	/**
+	 * for a given file with a list of words, perform some statistics and operations
+	 * @throws IOException
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 */
+	static void scoreWordList() throws IOException, ExecutionException, InterruptedException {
 		Scanner keyboard = new Scanner(System.in);
 		String filePath, mostPositiveWord="", mostNegativeWord="";
 		double mostPositiveScore=0, mostNegativeScore=99d;
 		double averageScore=0d;
 		int numWords = 0;
 		Hashtable<String, Future<Double>> wordScoreMap = new Hashtable<>();
+		List<String> positiveWords = new ArrayList<>();
+		List<String> negativeWords = new ArrayList<>();
 
+		/*
+		* get the input word list file
+		 */
 		System.out.print("Enter the name of the file with words you want to find average score for: ");
 		filePath = keyboard.nextLine();
 
 		File wordListFile = new File(filePath);
 		Scanner wordListScanner = new Scanner(wordListFile);
 
+		/*
+		* for each word in the file, submit a task to the thread pool
+		 */
 		ExecutorService executorService = Executors.newFixedThreadPool(8);
 		while(wordListScanner.hasNext()){
 			String word = wordListScanner.nextLine();
@@ -40,9 +52,15 @@ public class CS1ProjectStarterCode
 			wordScoreMap.put(word,future);
 		}
 
+		/*
+		* wait for all the tasks to be finished
+		 */
 		executorService.shutdown();
 		executorService.awaitTermination(100, TimeUnit.SECONDS);
 
+		/*
+		* go through the scores and do some operations
+		 */
 		for(Map.Entry<String, Future<Double>> entry : wordScoreMap.entrySet()){
 			double score = entry.getValue().get();
 			if (score>mostPositiveScore) {
@@ -54,10 +72,16 @@ public class CS1ProjectStarterCode
 				mostNegativeScore = score;
 			}
 
+			if (score>2.1) positiveWords.add(entry.getKey());
+			if (score<1.9) negativeWords.add(entry.getKey());
+
 			averageScore += score;
 			numWords += 1;
 		}
 
+		/*
+		* write out the summary
+		 */
 		if(numWords>0){
 			averageScore = averageScore/numWords;
 			System.out.println("The average score of the words in " + filePath + " is " +  decimalCustomFormat("##.##", averageScore));
@@ -73,8 +97,30 @@ public class CS1ProjectStarterCode
 		}else{
 			System.out.println("No words found in "+filePath);
 		}
+
+		/*
+		* write to separate files for positive and negative words
+		 */
+		PrintWriter positivePrintWriter = new PrintWriter("data/positive.txt");
+		PrintWriter negativePrintWriter = new PrintWriter("data/negative.txt");
+
+		for(String word:positiveWords){
+			positivePrintWriter.println(word);
+		}
+		positivePrintWriter.close();
+
+		for (String word:negativeWords){
+			negativePrintWriter.println(word);
+		}
+		negativePrintWriter.close();
 	}
 
+	/**
+	 * for each word compute the average score
+	 * @param word
+	 * @return
+	 * @throws FileNotFoundException
+	 */
 	static double computeAverageScore(String word) throws  FileNotFoundException
 	{
 		File reviewFile = new File("data/movieReviews.txt");
