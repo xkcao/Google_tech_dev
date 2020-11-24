ff74fec8eda2ea8edbf2fdd019d12ba1ea548d86
xiaokang <xiaokang.cao@ge.com>
2020-11-23 22:28:00 -0800

    GTD_2_21: first java implementation.

--- 2_Advanced_Programming/2_21_Rainwater/src/com/xiaokang/Main.java
+++ 2_Advanced_Programming/2_21_Rainwater/src/com/xiaokang/Main.java
@@ -1,8 +1,88 @@
 package com.xiaokang;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Result result = JUnitCore.runClasses(MainTest.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+
+    static int rainWaterVolume(int[] heightBar){
+        int leftIndex = smallestIndexMax(heightBar,0,heightBar.length-1);
+        int rightIndex = leftIndex;
+        int leftVolume=0;
+        int rightVolume=0;
+
+        while(leftIndex>0){
+            int secondLeftIndex = smallestIndexMax(heightBar,0,leftIndex-1);
+            for(int i=secondLeftIndex+1;i<leftIndex;i++){
+                leftVolume += heightBar[secondLeftIndex] - heightBar[i];
+            }
+
+            leftIndex = secondLeftIndex;
+        }
+
+        while ((rightIndex<heightBar.length-1)){
+            int secondRightIndex = largestIndexMax(heightBar,rightIndex+1,heightBar.length-1);
+            for (int i=rightIndex+1;i<secondRightIndex;i++){
+                rightVolume += heightBar[secondRightIndex] - heightBar[i];
+            }
+
+            rightIndex = secondRightIndex;
+        }
+
+        return (leftVolume + rightVolume);
+    }
+
+    /**
+     * find the smallest index with the max value in an array of integer between index start and end
+     * @param input input array
+     * @param start start index, 0 is the first element
+     * @param end end index
+     * @return
+     */
+    static int smallestIndexMax(int[] input, int start, int end){
+        if(start<0) start = 0;
+        if(end>input.length-1) end = input.length -1;
+
+        //initialize
+        int index = start;
+        for(int i=start;i<=end;i++){
+            if(input[i]>input[index]) index = i;
+        }
+
+        return index;
+    }
+
+    /**
+     * find the largest index with the max value in an array of integer between index start and end
+     * @param input input array
+     * @param start start index, 0 is the first element
+     * @param end end index
+     * @return
+     */
+    static int largestIndexMax(int[] input, int start, int end){
+        if(start<0) start = 0;
+        if(end>input.length-1) end = input.length -1;
+
+        //initialize
+        int index = start;
+
+        for(int i=start;i<=end;i++){
+            if(input[i]>=input[index]) index = i;
+        }
+
+        return index;
     }
 }
---
+++ 2_Advanced_Programming/2_21_Rainwater/src/com/xiaokang/MainTest.java
@@ -0,0 +1,26 @@
+package com.xiaokang;
+
+import org.junit.Test;
+
+import static com.xiaokang.Main.rainWaterVolume;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    public MainTest() {
+    }
+
+    @Test
+    public void rainWaterVolumeTest() {
+        int[] heightBar1 = {1,3,2,4,1,3,1,4,5,2,2,1,4,2,2};
+        int expectedResult1 = 15;
+        assertEquals( expectedResult1, rainWaterVolume(heightBar1));
+
+        int[] heightBar2 = {1,1,1};
+        int expectedResult2 = 0;
+        assertEquals( expectedResult2, rainWaterVolume(heightBar2));
+
+        int[] heightBar3 = {2,1,2};
+        int expectedResult3 = 1;
+        assertEquals( expectedResult3, rainWaterVolume(heightBar3));
+    }
+}
